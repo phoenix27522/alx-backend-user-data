5a2903ca6545bbb3910e4e1ca0001777f6f3cf8c
Nahom kiflu <kiflunahom1994@gmail.com>
2024-06-11 18:34:03 +0300

    task 0 - creating user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,11 +4,7 @@ Declare a SQLAlchemy model named 'User' corresponding to a
 database table named "users"
 """
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import (
-    Column,
-    Integer,
-    String,
-)
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
